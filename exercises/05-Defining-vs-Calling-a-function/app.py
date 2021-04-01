d99abe737d76a4c024998cd1483f1a33c73b4470
LucasDM1 <lucas19651@hotmail.com>
2021-04-01 23:15:02 +0000

    functions done

--- exercises/05-Defining-vs-Calling-a-function/app.py
+++ exercises/05-Defining-vs-Calling-a-function/app.py
@@ -1,4 +1,6 @@
 # Define the function called "multi" that expects 2 parameters:
+def multi(param1, param2):
+    return(param1*param2)
 
 # don't edit anything below this line
 return_value = multi(7,53812212)
--- exercises/10-Array-Methods/app.py
+++ exercises/10-Array-Methods/app.py
@@ -1,5 +1,8 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
-
+def sort_names(nameAr):
+    
+    nameAr.sort()
+    return nameAr
 
 print(sort_names(names))
